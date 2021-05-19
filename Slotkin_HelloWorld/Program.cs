b55647a2c18de101dea79cbba35bc045c57093ae
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-05-19 13:55:58 -0600

    Add project files.

---
+++ Slotkin_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slotkin_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
